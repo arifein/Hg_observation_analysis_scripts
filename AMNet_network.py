3055dafad1ca060ad5bd6227525457b38b7ab311
arifein <arifein@mit.edu>
2024-03-08 07:11:28 -0500

    Outputs hourly data for AMNet data

--- AMNet_network.py
+++ AMNet_network.py
@@ -29,51 +29,48 @@ def get_data_AMNet(df, station):
     elif (station == 'MS99'):
         df_station2 = df[df['SiteID']=='MS12'].copy()
         df_station =  pd.concat([df_station, df_station2]) 
+    elif (station == 'OH52'):
+        df_station2 = df[df['SiteID']=='oh52'].copy()
+        df_station2['SiteID'] = 'OH52' # correct lowercase error
+        df_station =  pd.concat([df_station, df_station2]) 
+    elif (station == 'NY20'):
+        df_station2 = df[df['SiteID']=='ny20'].copy()
+        df_station2['SiteID'] = 'NY20' # correct lowercase error
+        df_station =  pd.concat([df_station, df_station2]) 
     
     # Find where data is invalid
     temp = ((df_station['GEMVal']=='B') | (df_station['GEMVal']=='A')) & (df_station['GEM'] >= 0) # these are valid flags
     
     # filter data
     df_valid = df_station[temp].copy()
-
-    # load times
-    time_start = pd.to_datetime(df_valid['collStart'])
-    time_end = pd.to_datetime(df_valid['collEnd'])
-
-    # find midpoint time
-    time_mid = ((time_end - time_start)/2 + time_start).values 
-    df_valid['time_GEM'] = time_mid
-    
-    # get only years from time_mid, so that can use cutoff
-    # resample daily averages
-    df_valid_d = df_valid.set_index('time_GEM').resample('D').mean().dropna()
     
     # sort data by correct time
-    df_valid_d = df_valid_d.sort_index()
-        
-    return df_valid_d
+    df_valid = df_valid.sort_index()
+    
+    df_valid = df_valid[['SiteID','collStart','collEnd','GEM']]
+    df_valid = df_valid.rename(columns={"collStart": 'timeStart',"collEnd": 'timeEnd'})
+
+    return df_valid
 
 #%% Read all AMNet data
 # file name
 fn_all= '../../obs_datasets/GEM/AMNET-ALL-h.csv' # change relative path to AMNet data
-do = '../misc_Data/' # directory for outputted daily mean files
-
-# Names of long-term sites in the AMNet network 
-site_names = ['Birmingham', 'Pensacola','Yorkville','Mauna Loa','Piney Reservoir',
-              'Beltsville','Grand Bay NERR','New Brunswick','Bronx','Huntington Wildlife',
-              'Rochester','Athens','South Bass Island','Stillwell','Salt Lake City',
-              'Underhill','Horicon Marsh']
+do = 'all_data/' # directory for outputted daily mean files
 
 # Codes for the sites
-site_codes = ['AL19','FL96','GA40', 'HI00','MD08','MD98','MS99','NJ30',
-              'NY06','NY20','NY43','OH02','OH52','OK99','UT97','VT99','WI07',]
+site_codes = ['AK03','AL03','AL19','CA48','FL96','GA40','HI00','IL11',
+              'IN21','IN34','MD08','MD99','ME97','MI09','MS99',
+              'NH06','NJ05','NJ30','NJ54','NY06','NY20','NY43','NY95','OH02',
+              'OH52','OK99','PA13','UT96','UT97','VT99','WI07','WV99',
+              'AK95','IL69','NY98', 'MA22']
+      
 # read file with all hourly data
 df_all = pd.read_csv(fn_all)
-for i in range(len(site_names)):
-    print("Loading site: " + site_names[i])
+for i in range(len(site_codes)):
+    print("Loading site: " + site_codes[i])
     # get data from sites at daily time resolution
     df = get_data_AMNet(df_all, site_codes[i])
     # output csv of daily averages
-    fo = do + site_codes[i] + '_d.csv'
-    df.to_csv(fo)
+    fo = do + site_codes[i] + '_all.csv'
+    df.to_csv(fo, index=False)
     
