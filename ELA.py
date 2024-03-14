355fdbdaf346b6877c5e4d0c95af8ea5c83452fe
arifein <arifein@mit.edu>
2024-03-14 04:01:04 -0400

    added ELA files

--- ELA.py
+++ ELA.py
@@ -8,6 +8,10 @@ Load Hg0 observation data courtesy from Experimental Lakes Area courtesy of Vinc
 #%% Import packages
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('Agg')
+
 #%% Functions used for analysis
 def load_data_misc(site, fn):
     """Load the data over all years for the site
@@ -28,10 +32,8 @@ def load_data_misc(site, fn):
     time_start = df['Sample date/Time start']
     time_end = df['Sample date/Time end']
 
-    # find midpoint time
-    time_mid = ((time_end - time_start)/2 + time_start).values
-    df['time_mid'] = time_mid
-    
+    df = df.rename(columns={"Sample date/Time start": "timeStart", 
+                            "Sample date/Time end": "timeEnd"})
     return df
 
 def get_data_misc(site, dn):
@@ -59,15 +61,17 @@ def get_data_misc(site, dn):
                             "RGM (pg/m^3)": "RGM_pg_m3",
                             "PHg (pg/m^3)": "PHg_pg_m3"})
             
-    # resample daily averages
-    df_d = df.set_index('time_mid').resample('D').mean().dropna()
+    df['SiteID'] = 'ELA'
+    # remove NAN 
+    df_d = df.dropna()
+    df_d = df_d[['SiteID','timeStart','timeEnd','GEM']]
                     
     return df_d
 
 #%% Read misc data
 stations_all = ['ELA']
 dn = '../../obs_datasets/GEM/' # directory for misc files, change to your path
-do = '../misc_Data/' # directory for outputted daily mean files
+do = 'all_data/' # directory for outputted files
 
 # run loop over sites to load and process data
 for station in stations_all:
@@ -75,5 +79,16 @@ for station in stations_all:
     # get daily data from site
     df = get_data_misc(station, dn)
     # output csv of daily averages
-    fo = do + station + '_d.csv'
-    df.to_csv(fo)
+    fo = do + station + '_all.csv'
+    df.to_csv(fo, index=False)
+    # add plot
+    f,  axes = plt.subplots(1,1, figsize=[18,10],
+                        gridspec_kw=dict(hspace=0.35, wspace=0.35))
+    axes.plot(pd.to_datetime(df.timeStart), df['GEM'], '.', ms=1)
+    axes.set_title(station)
+    axes.set_ylabel('GEM (ng m$^{-3}$)')
+    # set limits, for better visualization
+    pct_995 = np.percentile( df['GEM'],99.5)
+    pct_005 = np.percentile( df['GEM'],00.5)
+    axes.set_ylim(top = pct_995, bottom = pct_005)
+    f.savefig('Figures/'+station+'_all.pdf',bbox_inches = 'tight')
